f36fdf7a4ea37b3425a7dd8d435c1a3afbb3ee94
HeidyPrieto <choripapa2050@gmail.com>
2024-02-20 13:36:09 -0500

    Backend(Agregations Package NuGets in Test integration 3)

--- StartingProject/EmployeesApp/EmployeesApp.Tests/obj/Debug/net8.0/EmployeesApp.Tests.AssemblyInfo.cs
+++ StartingProject/EmployeesApp/EmployeesApp.Tests/obj/Debug/net8.0/EmployeesApp.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeesApp.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af55981a258de096429544ed9e833ed98bb30294")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeesApp.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeesApp.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
